c484f744ca2d290274ce670b43ffb3adaea4f1ed
rrta911 <139085730+rrta911@users.noreply.github.com>
2024-04-30 22:15:15 +0700

    Update Day11.sql

--- Day11.sql
+++ Day11.sql
@@ -5,3 +5,16 @@ INNER JOIN COUNTRY b ON a.countrycode = b.code
 GROUP BY b.continent
 
 --baitap 2 
+SELECT ROUND(CAST(COUNT(b.email_id) AS DECIMAL)/ COUNT(DISTINCT a.email_id),2) activation_rate
+FROM emails a
+LEFT JOIN texts b ON a.email_id = b.email_id
+                  AND b.signup_action = 'Confirmed'
+
+--baitap 3 
+
+--baitap 4 
+
+MID-COURSE 
+
+  baitap1: 
+  SELECT * 
