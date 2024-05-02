70efd1572642666b1efae1a9a4705fc5fbcf7763
rrta911 <139085730+rrta911@users.noreply.github.com>
2024-05-02 11:28:07 +0700

    Update Day11.sql

--- Day11.sql
+++ Day11.sql
@@ -35,7 +35,79 @@ LEFT JOIN texts b ON a.email_id = b.email_id
   ORDER BY employee_id
 
 --baitap 6 
+      SELECT p.product_name, SUM(o.unit)  unit 
+  FROM products p
+  JOIN orders o 
+  ON p.product_id = o.product_id 
+    WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+  GROUP BY p.product_name
+  HAVING SUM(o.unit) >=100
+--baitap 7 
+        SELECT p.page_id
+  FROM pages p
+  LEFT JOIN page_likes l 
+  ON p.page_id = l.page_id
+  GROUP BY(p.page_id)
+  HAVING COUNT(l.page_id) = 0
+      
 MID-COURSE 
 
-  baitap1: 
-  SELECT * 
+--baitap1: 
+  SELECT replacement_cost
+  FROM public.film
+  ORDER BY replacement_cost ASC;
+
+--baitap 2
+  SELECT 
+	SUM(CASE WHEN replacement_cost BETWEEN 9.99 AND 19.99 THEN 1 ELSE 0 END) low
+	--CASE WHEN replacement_cost BETWEEN 20.00 AND 24.99 THEN replacement_cost ELSE 0 END medium,
+	--CASE WHEN replacement_cost BETWEEN 25.00 AND 29.99 THEN replacement_cost ELSE 0 END high
+  FROM film
+
+--baitap 3 
+  SELECT f.title, f.length, c.name
+  FROM film f
+  JOIN film_category fc ON f.film_id = fc.film_id
+  JOIN category c ON fc.category_id = c.category_id AND c.name IN ('Drama', 'Sports')
+  ORDER BY f.length DESC;
+
+--baitap 4 
+  SELECT f.title, f.length, c.name
+  FROM film f
+  JOIN film_category fc ON f.film_id = fc.film_id
+  JOIN category c ON fc.category_id = c.category_id AND c.name IN ('Drama', 'Sports')
+  ORDER BY f.length DESC;
+
+--baitap 5 
+  SELECT CONCAT(ac.first_name, ' ', ac.last_name), COUNT(f.title) 
+  FROM public.actor ac 
+  JOIN film_actor fc ON ac.actor_id = fc.actor_id
+  JOIN film f ON f.film_id = fc.film_id
+  GROUP BY ac.first_name, ac.last_name
+  ORDER BY COUNT(f.title) DESC
+
+--baitap 6
+  SELECT COUNT(*)
+  FROM address a 
+  LEFT JOIN customer c ON a.address_id = c.address_id WHERE c.address_id IS NULL
+
+--baitap 7 
+  SELECT city || ':', SUM(p.amount)
+  FROM city as ci
+  LEFT JOIN address ad ON ci.city_id = ad.city_id
+  LEFT JOIN customer c ON ad.address_id = c.address_id 
+  LEFT JOIN payment p ON c.customer_id = p.customer_id 
+  GROUP BY city
+  ORDER BY SUM(p.amount) DESC
+
+--baitap 8 
+  SELECT co.country || ',' || ci.city, SUM(p.amount)
+FROM city as ci
+LEFT JOIN country co ON ci.country_id = co.country_id
+LEFT JOIN address ad ON ci.city_id = ad.city_id
+LEFT JOIN customer c ON ad.address_id = c.address_id 
+LEFT JOIN payment p ON c.customer_id = p.customer_id 
+WHERE country = 'United States'
+GROUP BY co.country, city
+ORDER BY SUM(p.amount) DESC
+
