fc8a08474399405acd28207b4986df5d9ca4b3aa
rrta911 <139085730+rrta911@users.noreply.github.com>
2024-05-10 17:45:19 +0700

    Update Project1.sql

--- Project1.sql
+++ Project1.sql
@@ -61,3 +61,49 @@ select * from sales_dataset_rfm_prj
 	SET MONTH_ID
 	UPDATE sales_dataset_rfm_prj
 	SET  YEAR_ID = EXTRACT(YEAR from orderdate)
+
+--câu 5 
+	WITH twt_min_max_value AS(
+	SELECT Q1 - 1.5*IQR min_value,
+	Q3 + 1.5*IQR max_value
+	FROM(
+	select 
+	percentile_cont(0.25) WITHIN GROUP (ORDER BY QUANTITYORDERED) Q1,
+	percentile_cont(0.75) WITHIN GROUP (ORDER BY QUANTITYORDERED) Q3,
+	percentile_cont(0.75) WITHIN GROUP (ORDER BY QUANTITYORDERED) - percentile_cont(0.25) WITHIN GROUP (ORDER BY QUANTITYORDERED) IQR 
+	from sales_dataset_rfm_prj) as a)
+	--tien hanh loc outlier 
+	/*select * from sales_dataset_rfm_prj
+	where QUANTITYORDERED < (select min_value from twt_min_max_value)
+	or QUANTITYORDERED > (select max_value from twt_min_max_value)*/
+		-- delete data 
+		DELETE FROM sales_dataset_rfm_prj
+		WHERE QUANTITYORDERED IN (select QUANTITYORDERED from sales_dataset_rfm_prj
+	where QUANTITYORDERED < (select min_value from twt_min_max_value)
+	or QUANTITYORDERED > (select max_value from twt_min_max_value))
+--cach 2 
+	-- z-score = (x-avg)/stddev ( viet cte tim truoc)
+	with cte as
+(
+	select QUANTITYORDERED,
+		(select avg(QUANTITYORDERED) 
+		from sales_dataset_rfm_prj) as avg, 
+		(select stddev(QUANTITYORDERED) 
+		from sales_dataset_rfm_prj) as stddev
+	from sales_dataset_rfm_prj ) 
+	--loc outlier
+, TWT_outlier as(
+select  QUANTITYORDERED, (QUANTITYORDERED-avg)/stddev as z_score
+from cte
+where abs((QUANTITYORDERED-avg)/stddev) >3)
+	--update
+UPDATE sales_dataset_rfm_prj
+SET QUANTITYORDERED=(select avg(QUANTITYORDERED)
+from sales_dataset_rfm_prj)
+WHERE QUANTITYORDERED IN (select QUANTITYORDERED from twt_outlier)
+
+
+--câu 6 
+
+ALTER TABLE sales_dataset_rfm_prj
+RENAME TO SALES_DATASET_RFM_PRJ_CLEAN
