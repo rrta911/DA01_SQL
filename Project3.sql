fa262951887e66c2e65da9c1f91a492cf2c88b56
rrta911 <139085730+rrta911@users.noreply.github.com>
2024-05-17 20:32:04 +0700

    Update Project3.sql

--- Project3.sql
+++ Project3.sql
@@ -54,3 +54,38 @@ WHERE month_id =11 AND revenue IN (SELECT max(revenue) FROM revenue_month  GROUP
  WHERE country = 'UK'
 group by year_id, productline) as t
 where Rankk =1
+
+--baitap 5 
+/* B1: Tính giá trị R-F-M*/
+with customer_rfm as(
+select 
+	customername,
+	current_date - Max(orderdate) R,
+	count(distinct ordernumber) F,
+	sum(sales) as M 
+from public.sales_dataset_rfm_prj_clean
+GROUP BY customername),
+
+/* B2: Phân nhóm khách hàng thành các khoảng 1 -5 */
+	 rfm_score as(
+	select customername,
+	ntile(5) OVER(ORDER BY R DESC) R_score,
+	ntile(5) OVER(ORDER BY F DESC) F_score,
+	ntile(5) OVER(ORDER BY M DESC) M_score
+	from customer_rfm)
+
+/* B3: Phân nhóm theo tổ hợp */
+	,rfm_final as(
+	SELECT customername,
+	CAST(R_score AS varchar) || CAST(F_score AS varchar) || CAST(M_score AS varchar) as answer
+	FROM rfm_score )
+	
+	SELECT segment, customername from (
+	SELECT *
+	FROM rfm_final a
+	JOIN public.segment_score b on a.answer = b.scores)a
+	WHERE segment IN ('Potential Loyalist', 'Promising')
+	GROUP BY segment,customername
+	
+
+	
